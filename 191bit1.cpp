0ca68656920b9a01e47d867d0fc0d935d08e6456
Priyanshi Patidar <priyanshipatidar967@gmail.com>
2026-01-06 20:45:02 +0530

    solved problem 191

---
+++ 191bit1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+    int count = 0;
+    while(n!=0){
+    if(n%2!=0){
+        count++;}
+    n=n/2;
+    }
+    cout<<count;
+
+}
