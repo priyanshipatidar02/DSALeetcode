94f1112cf1c3d51fc790d56e85be198edfa4799a
Priyanshi Patidar <priyanshipatidar967@gmail.com>
2026-01-11 21:00:14 +0530

    Sync files and deletions

--- Arrays/UniqueElement.cpp
+++ Arrays/UniqueElement.cpp
@@ -23,7 +23,7 @@ int main() {
   cout << "enter size of an array : ";
   cin >> size;
   int arr[100];
-  cout << "eneter elements of an array: ";
+  cout << "enter elements of an array: ";
   for (int i = 0; i < size; i++) {
     cin >> arr[i];
   }
