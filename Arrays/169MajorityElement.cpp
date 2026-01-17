1805d23ac937775b17f5c3ac621304f661060fdc
Priyanshi Patidar <priyanshipatidar967@gmail.com>
2026-01-17 23:21:23 +0530

    solved problem no. 169 on leetcode

---
+++ Arrays/169MajorityElement.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+  int majorityElement(vector<int> &nums) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < nums.size(); i++) {
+      mpp[nums[i]]++;
+      if (mpp[nums[i]] > (nums.size() / 2)) {
+        return nums[i];
+      }
+    }
+    return -1;
+  }
+};
