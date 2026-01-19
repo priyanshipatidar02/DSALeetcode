dc9aa1f2af117798493e62870242a7f3d60176e9
Priyanshi Patidar <priyanshipatidar967@gmail.com>
2026-01-19 22:40:48 +0530

    solved problem 7 on leetcode

--- 7ReverseInteger.cpp
+++ 7ReverseInteger.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int x;
-    cout<<"enter x :";
-    cin>>x;
-   long long rev = 0;
+int main() {
+  int x;
+  cout << "enter x :";
+  cin >> x;
+  long long rev = 0;
 
-        while(x != 0){
-            int digit = x % 10;
-            rev = rev * 10 + digit;
-            x /= 10;
+  while (x != 0) {
+    int digit = x % 10;
+    rev = rev * 10 + digit;
+    x /= 10;
 
-            if(rev > INT_MAX || rev < INT_MIN)
-                return 0;
-        }
-
-        return (int)rev;
-
-    
+    if (rev > INT_MAX || rev < INT_MIN)
+      return 0;
+  }
+  return (int)rev;
 }
